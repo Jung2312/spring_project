1f722f66a559c337c74659bf79b6c543a42c241e
eple0117 <sangwoo239@naver.com>
2024-11-19 17:41:42 +0900

    mainpage, signup
    커뮤 홈, 쇼핑 홈, 회원가입

---
+++ src/main/front/src/user/signup.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import logo from '../img/myhouse_logo.png';
+import css from '../css/signup.css';
+
+function Signup() {
+    return (
+        <div className="container">
+            <div className="logo">
+                <a href=""><img src={logo} alt="나만의집 로고"/></a>
+                <span className="logo-name">나만의집</span>
+            </div>
+            <div className="signup">
+                <span className="signup-text">회원가입</span>
+            </div>
+            <div className="id-field">
+                <span className="text">아이디</span>
+                <p>다른 유저와 겹치지 않도록 입력해주세요. (2~20자)</p>
+                <input type="text" name="id" id="id-field" placeholder="아이디"/>
+            </div>
+            <div className="pwd-field">
+                <span className="text">비밀번호</span>
+                <p>영문, 숫자를 포함한 8자 이상의 비밀번호를 입력해주세요.</p>
+                <input type="password" name="pwd" id="pwd-field" placeholder="비밀번호"/>
+            </div>
+            <div className="pwdchk-field">
+                <span className="text">비밀번호 확인</span>
+                <input type="password" name="pwdchk" id="pwdchk-field" placeholder="비밀번호 확인"/>
+            </div>
+            <div className="name-field">
+                <span className="text">이름</span>
+                <input type="text" name="name" id="name-field" placeholder="이름"/>
+            </div>
+            <div className="phone-field">
+                <span className="text">전화번호</span>
+                <input type="text" name="phone" id="phone-field" placeholder="전화번호"/>
+            </div>
+            <div className="email-field">
+                <span className="text">이메일</span>
+                <input type="email" name="email" id="email-field" placeholder="이메일"/>
+            </div>
+            <div className="post-field" style={{ marginBottom: '0px' }}>
+                <button type="button" style={{ marginBottom: '10px' }}>우편번호 검색</button>
+                <input style={{ marginBottom: '10px' }} type="text" name="post" id="post-field" placeholder="우편번호"/>
+            </div>
+            <div className="address-field">
+                <input type="text" name="address" id="address-field" placeholder="주소"/>
+            </div>
+            <button type="submit">가입하기</button>
+        </div>
+    );
+}
+
+export default Signup;
