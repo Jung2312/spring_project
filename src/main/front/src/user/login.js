af504dedafe3c8ab4acca7cec6171000e8184f6b
JeongminHW <114545423+JeongminHW@users.noreply.github.com>
2024-11-19 09:41:24 +0900

    update front
    update front

---
+++ src/main/front/src/user/login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../img/myhouse_logo.png'
+import css from '../css/login.css'
+
+function Login() {
+    return (
+        <div className="container">
+            <div className="logo">
+                <a href=""><img src={logo}/></a>
+                <span className="logo-name">나만의집</span>
+            </div>
+            <div className="login-field">
+                <input type="text" name="id" id="id-field" placeholder="아이디"/>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
