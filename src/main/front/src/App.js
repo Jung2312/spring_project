15d1ea613474720b3461488a78a9ea5969f7bf64
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2024-12-12 02:59:55 +0900

    post detail

--- src/main/front/src/App.js
+++ src/main/front/src/App.js
@@ -26,7 +26,7 @@ import Profile from "./user/profile";
 import ShoppingInformation from "./shopping/shoppingInformation";
 import ShoppingBest from "./shopping/shoppingBest";
 import Payment from "./shopping/payment";
-import RecommendDetail from "./community/recommendDetail";
+import PostDetail from "./community/recommendDetail";
 import ContestRegistration from "./admin/addContest";
 import ContestEdit from "./admin/editContest";
 
@@ -67,6 +67,7 @@ function AppRouter() {
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/storeSignup" element={<StoreSignup />} />
+            <Route path="/community/recommendDetail/:postnum" element={<PostDetail />} />
             <Route path="/community/recommend" element={<Recommend />} />
             <Route path="/recommend/post" element={<RecommendPostPage />} />
             <Route path="/cart" element={<Cart />} />
@@ -95,34 +96,6 @@ function AppRouter() {
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Navigate to="/main" replace />} />
-                <Route path="/contest" element={<ContestApply />} />
-                <Route path="/contest/champion" element={<ContestChampionship />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/storeSignup" element={<StoreSignup />} />
-                <Route path="/community/recommend" element={<Recommend />} />
-                <Route path="/community/recommendDetail/:postnum" element={<RecommendDetail />} />
-                <Route path="/recommend/post" element={<RecommendPostPage />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/myPage/setting" element={<MyPageSet />} />
-                <Route path="/shopping/shoppingHome" element={<ShoppingHome />} />
-                <Route path="/main" element={<MainPage />} />
-                <Route path="/storeManagement" element={<StoreManagement />} />
-                <Route path="/myPage/profile" element={<MyPage />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/followPage" element={<FollowPage />} />
-                <Route path="/contest/post" element={<ContestPostPage />} />
-                <Route path="/contest/postDetail/:joinnum" element={<ContestPostDetailPage />} />
-                <Route path="/contest/notice" element={<ContestNotice />} />
-                <Route path="/shopping/shoppingCategory" element={<ShoppingCategory />} />
-                <Route path="/myPage/myShoppingPage" element={<MyShoppingPage />} />
-                <Route path="/productDetail" element={<ProductDetail />} />
-                <Route path="/shoppingInformation/:storename" element={<ShoppingInformation />} />
-                <Route path="/shopping/shoppingBest" element={<ShoppingBest />} />
-                <Route path="/payment" element={<Payment />} />
-            </Routes>
             <AppRouter />
         </BrowserRouter>
     );
