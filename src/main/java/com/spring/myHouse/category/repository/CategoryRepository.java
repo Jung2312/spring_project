f7383f62be0dd96555284eb1bb4a0af7dc82a8ed
JUNG <20212856@office.deu.ac.kr>
2024-12-12 21:00:36 +0900

    feat : mainpage Category move
    feat : mainpage Category move

--- src/main/java/com/spring/myHouse/category/repository/CategoryRepository.java
+++ src/main/java/com/spring/myHouse/category/repository/CategoryRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface CategoryRepository extends CrudRepository<Category, Integer> {
     // majorcategory로 서브카테고리 목록을 조회
-    List<Category> findByMajorcategory(String majorCategory);
+    List<Category> findAllByMajorcategory(String majorCategory);
 
     // 모든 카테고리 목록을 조회
     List<Category> findAll();
